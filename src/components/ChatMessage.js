4ea775b0fd1ee253de2acb542ac22a9ec3ee38b3
Balaji1472 <balajirama.2005@gmail.com>
2025-04-13 12:07:50 +0530

    cleared error

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,22 +1,3 @@
-// import React from 'react';
-// import styles from './ChatMessage.module.css';
-
-// export function ChatMessage({ message }) {
-//   const isUser = message.role === 'user';
-  
-//   return (
-//     <div className={`${styles.messageContainer} ${isUser ? styles.userMessage : styles.assistantMessage}`}>
-//       <div className={styles.avatar}>
-//         {isUser ? '👤' : '🤖'}
-//       </div>
-//       <div className={styles.messageContent}>
-//         <div className={styles.messageText}>{message.content}</div>
-//       </div>
-//     </div>
-//   );
-// }
-
-
 
 // File: src/components/ChatMessage.js
 import React from 'react';
